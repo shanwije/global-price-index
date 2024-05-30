0260d604d48620d09add2cd263d16b611c119500
Shan Wijenayaka <shanwije.dev@gmail.com>
2024-05-30 15:09:00 +0800

    validate pipeline and exception filters added

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,31 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { LogLevel } from '@nestjs/common/services/logger.service';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './config/exceptions/AllExceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const logger = new Logger('Bootstrap');
 
-  const port = configService.get<number>('port') || 3000;
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
+  );
+  app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
 
-  const logLevel = (configService.get<string>('logLevel') as LogLevel) || 'log';
+  const port = configService.get<number>('port');
+
+  const logLevel = configService.get<string>('logLevel') as LogLevel;
 
   Logger.overrideLogger([logLevel]);
 
   const config = new DocumentBuilder()
     .setTitle('Global Price Index API')
     .setDescription(
-      'API to provide the global price index for the BTC/USDT trading pair from Binance, Kraken, and Huobi exchanges.',
+      'API to provide the global price index for the BTC/USDT trading pair from Binance, Kraken, and Huobi exchanges.'
     )
     .setVersion('1.0')
     .build();
